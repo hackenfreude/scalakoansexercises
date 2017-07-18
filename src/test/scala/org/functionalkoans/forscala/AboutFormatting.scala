db560ce1d96ddf01bfd6e2f45a2bedad5bd39c03
hackenfreude <hackenfreude@users.noreply.github.com>
2017-07-17 22:58:07 -0700

    through AboutRange

--- src/test/scala/org/functionalkoans/forscala/AboutFormatting.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutFormatting.scala
@@ -7,7 +7,7 @@ class AboutFormatting extends KoanSuite with Matchers {
 
   koan("String can be placed in format") {
     val s = "Hello World"
-    "Application %s".format(s) should be(__)
+    "Application %s".format(s) should be("Application Hello World")
   }
 
   koan("Character Literals can be an a single character") {
@@ -17,8 +17,8 @@ class AboutFormatting extends KoanSuite with Matchers {
     //format(a) is a string format, meaning the "%c".format(x)
     //will return the string representation of the char.
 
-    "%c".format(a) should be(__)
-    "%c".format(b) should be(__)
+    "%c".format(a) should be("a")
+    "%c".format(b) should be("B")
 
   }
 
@@ -30,35 +30,35 @@ class AboutFormatting extends KoanSuite with Matchers {
     val e = '\"'
     val f = '\\'
 
-    "%c".format(c) should be(__)
-    "%c".format(d) should be(__)
-    "%c".format(e) should be(__)
-    "%c".format(f) should be(__)
+    "%c".format(c) should be("a")
+    "%c".format(d) should be("a")
+    "%c".format(e) should be("\"")
+    "%c".format(f) should be("\\")
   }
 
   koan("Formatting can also include numbers") {
     val j = 190
-    "%d bottles of beer on the wall" format j - 100 should be(__)
+    "%d bottles of beer on the wall" format j - 100 should be("90 bottles of beer on the wall")
   }
 
   koan("Formatting can be used for any number of items, like a string and a number") {
     val j = 190
     val k = "vodka"
 
-    "%d bottles of %s on the wall".format(j - 100, k) should be(__)
+    "%d bottles of %s on the wall".format(j - 100, k) should be("90 bottles of vodka on the wall")
   }
 
   koan("Formatting can be done inline using the 's' macro on a string") {
     val j = 190
     val k = "vodka"
 
-    s"$j bottles of $k on the wall" should be(__)
+    s"$j bottles of $k on the wall" should be("190 bottles of vodka on the wall")
   }
 
   koan("Formatting with the 's' macro can also be used for expressions") {
     val j = 190
     val k = "vodka"
 
-    s"${j - 100} bottles of $k on the wall" should be(__)
+    s"${j - 100} bottles of $k on the wall" should be("90 bottles of vodka on the wall")
   }
 }
