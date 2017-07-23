9b417d640d8866461b75e2cfd06b4f378b850637
hackenfreude <hackenfreude@users.noreply.github.com>
2017-07-23 14:08:19 -0700

    through AboutEnumerations

--- src/test/scala/org/functionalkoans/forscala/AboutByNameParameter.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutByNameParameter.scala
@@ -27,7 +27,7 @@ class AboutByNameParameter extends KoanSuite {
       14 + 15
     }
 
-    y should be(__)
+    y should be(Right(29))
   }
 
 
@@ -51,7 +51,7 @@ class AboutByNameParameter extends KoanSuite {
       49 + 20
     }
 
-    y should be(__)
+    y should be(Right(69))
   }
 
   koan("""By name parameters can also be used with an Object and apply to make interesting block-like calls""") {
@@ -65,6 +65,6 @@ class AboutByNameParameter extends KoanSuite {
       x ++ z //concatenate the strings
     }
 
-    result should be(__)
+    result should be("retzelpay")
   }
 }
--- src/test/scala/org/functionalkoans/forscala/AboutTypeTags.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTypeTags.scala
@@ -13,13 +13,13 @@ class AboutTypeTags extends KoanSuite {
       | before it erased by the VM by using an implicit TypeTag argument.""") {
     def inspect[T](l: List[T])(implicit tt: TypeTag[T]) = tt.tpe.typeSymbol.name.decodedName.toString
     val list = 1 :: 2 :: 3 :: 4 :: 5 :: Nil
-    inspect(list) should be(__)
+    inspect(list) should be("Int")
   }
 
   koan("""TypeTags can also be """) {
     def inspect[T: TypeTag](l: List[T]) = typeOf[T].typeSymbol.name.decodedName.toString
     val list = 1 :: 2 :: 3 :: 4 :: 5 :: Nil
-    inspect(list) should be(__)
+    inspect(list) should be("Int")
   }
 
   koan(
@@ -30,6 +30,6 @@ class AboutTypeTags extends KoanSuite {
       def +(t: T) = "1 %s has been added".format(tt.tpe.typeSymbol.name.decodedName.toString)
     }
     val candyBarrel = new Barrel[Candy]
-    (candyBarrel + new Candy) should be(__)
+    (candyBarrel + new Candy) should be("1 Candy has been added")
   }
 }
